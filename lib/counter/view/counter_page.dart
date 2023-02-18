f367e585a4d5661b0104d222d2515ca7596ecab5
Hope Kagame <kagame458@protonmail.com>
2023-02-19 01:32:16 +0300

    Show current count on screen

--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -1,5 +1,6 @@
 import 'package:ds_client/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class CounterPage extends StatefulWidget {
   const CounterPage({super.key});
@@ -9,10 +10,25 @@ class CounterPage extends StatefulWidget {
 }
 
 class _CounterPageState extends State<CounterPage> {
+  late WebSocketChannel _channel;
+  late String _count;
 
   @override
   void initState() {
+    _count = '0';
+    _channel = WebSocketChannel.connect(Uri.parse('ws://localhost:8080/ws'));
+    _channel.stream.listen(
+      (data) => setState(() {
+        _count = data as String;
+      }),
+    );
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _channel.sink.close();
+    super.dispose();
   }
 
   @override
@@ -20,18 +36,18 @@ class _CounterPageState extends State<CounterPage> {
     final l10n = context.l10n;
     return Scaffold(
       appBar: AppBar(title: Text(l10n.counterAppBarTitle)),
-      body: const Center(child: CounterText()),
+      body: Center(child: CounterText(count: _count)),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () => _channel.sink.add('__increment__'),
             child: const Icon(Icons.add),
           ),
           const SizedBox(height: 8),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: () => _channel.sink.add('__decrement__'),
             child: const Icon(Icons.remove),
           ),
         ],
@@ -41,12 +57,16 @@ class _CounterPageState extends State<CounterPage> {
 }
 
 class CounterText extends StatelessWidget {
-  const CounterText({super.key});
+  const CounterText({
+    required this.count,
+    super.key,
+  });
+
+  final String count;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    const count = 10;
-    return Text('$count', style: theme.textTheme.displayLarge);
+    return Text(count, style: theme.textTheme.displayLarge);
   }
 }
