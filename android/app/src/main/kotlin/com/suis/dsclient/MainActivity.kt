ec47280146785c7f37299dfe8c6921de1ffae964
Hope Kagame <kagame458@protonmail.com>
2023-02-18 21:30:49 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/suis/dsclient/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.suis.dsclient.ds_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:ds_client/app/app.dart';
+import 'package:ds_client/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
