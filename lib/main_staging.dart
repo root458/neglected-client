bb89b63c77f2ac73442db067abcffc06593bd3c5
Hope Kagame <kagame458@protonmail.com>
2023-02-20 14:40:04 +0300

    Update main files to use unique ID

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,11 +1,14 @@
 import 'package:ds_client/app/app.dart';
 import 'package:ds_client/bootstrap.dart';
 import 'package:ds_client/colorswitch/utils.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
+  final uuid = const Uuid().v4();
   ColorSwitchConfig(
     values: ColorSwitchValues(
-      baseDomain: 'ws://localhost:8080/ws',
+      baseDomain: 'ws://localhost:8080/ws?id=$uuid',
+      clientUniqueID: uuid,
     ),
   );
   bootstrap(() => const App());
